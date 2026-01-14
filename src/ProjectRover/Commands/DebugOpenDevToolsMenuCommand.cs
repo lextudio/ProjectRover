4d5e01d79642c21c80ee7e45617ada6e1cb25120
Lex Li <support@lextm.com>
2026-01-14 10:05:57 -0500

    Disable devtools menu item in release mode

--- src/ProjectRover/Commands/DebugOpenDevToolsMenuCommand.cs
+++ src/ProjectRover/Commands/DebugOpenDevToolsMenuCommand.cs
@@ -6,7 +6,9 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 
+#if DEBUG
 [ExportMainMenuCommand(ParentMenuID = "_Help", Header = "Open DevTools", InputGestureText = "F12", MenuOrder = 1000)]
+#endif
 public class DebugOpenDevToolsMenuCommand : ICommand
 {
     public event EventHandler? CanExecuteChanged;
